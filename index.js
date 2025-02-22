44a1cc974ce889f2c0bee045223820652132ec98
red-one12 <radoanahmed1242000@gmail.com>
2025-02-22 16:47:03 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const database = client.db("TaskManagerDB");
@@ -161,8 +161,8 @@ app.delete('/tasks/:id', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
